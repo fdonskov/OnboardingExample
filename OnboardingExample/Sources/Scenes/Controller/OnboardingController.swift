104e84a4f8e7cf9ee34af09572c202793977503a
Fedor Donskov <fedya_donskov@mail.ru>
2022-06-15 18:53:41 +0300

    Made a calculated property to transform the parent view

--- OnboardingExample/Sources/Scenes/Controller/OnboardingController.swift
+++ OnboardingExample/Sources/Scenes/Controller/OnboardingController.swift
@@ -8,11 +8,20 @@
 import UIKit
 
 class OnboardingController: UIViewController {
-
+    
+    // Это вычисляемое свойство преобразует тип родительской view в OnboardingView
+    // Это делается чтобы мы в будущем могли из Controller'a обращаться к элементам View
+    private var onboardingView: OnboardingView? {
+        guard isViewLoaded else { return nil }
+        return view as? OnboardingView
+    }
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .red
+        // Присваиваем значение View наш созданный класс OnboardingView()
+        view = OnboardingView()
     }
 
 
--- OnboardingExample/Sources/Scenes/View/OnboardingView.swift
+++ OnboardingExample/Sources/Scenes/View/OnboardingView.swift
@@ -5,4 +5,8 @@
 //  Created by Федор Донсков on 15.06.2022.
 //
 
-import Foundation
+import UIKit
+
+class OnboardingView: UIView {
+    
+}
