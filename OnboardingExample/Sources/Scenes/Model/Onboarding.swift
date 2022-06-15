0ff4bf2931646388ebfd9ad6c906210ab61d2a0d
Fedor Donskov <fedya_donskov@mail.ru>
2022-06-15 20:25:13 +0300

    Implementation of the model

---
+++ OnboardingExample/Sources/Scenes/Model/Onboarding.swift
@@ -0,0 +1,15 @@
+//
+//  Onboarding.swift
+//  OnboardingExample
+//
+//  Created by Федор Донсков on 15.06.2022.
+//
+
+import Foundation
+
+struct Onboarding {
+    let index: Int
+    let title: String
+    let description: String
+    let imageName: String
+}
