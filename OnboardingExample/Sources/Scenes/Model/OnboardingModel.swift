3cb17fa5658a8f171905e50ec5c4faff0f4e826e
Fedor Donskov <fedya_donskov@mail.ru>
2022-06-15 14:12:53 +0300

    Add OnboardingModel, OnboardingView, OnboardingController

---
+++ OnboardingExample/Sources/Scenes/Model/OnboardingModel.swift
@@ -0,0 +1,8 @@
+//
+//  OnboardingModel.swift
+//  OnboardingExample
+//
+//  Created by Федор Донсков on 15.06.2022.
+//
+
+import Foundation
---
+++ OnboardingExample/Sources/Scenes/View/OnboardingView.swift
@@ -0,0 +1,8 @@
+//
+//  OnboardingView.swift
+//  OnboardingExample
+//
+//  Created by Федор Донсков on 15.06.2022.
+//
+
+import Foundation
